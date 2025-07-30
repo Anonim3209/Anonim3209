9444351cff35677ee52f2fdd76314d236f46d7b2
Skirk <86675968+Anonim3209@users.noreply.github.com>
2025-07-30 16:21:28 +0500

    Update router.js

--- router.js
+++ router.js
@@ -6,37 +6,17 @@ import htmlQuestionController from "./Controllers/htmlQuestionController.js";
 
 const router = new Router();
 
-// Панель администратора
 router.get("/admin-panel", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-// Страница с вопросами и входами
 router.get("/questions", (_, res) => {
   res.sendFile(path.join(__dirname, "public", "questionsAndEntrances.html"));
 });
-
-// 📌 Новый чистый маршрут для симулятора LMS
-router.get("/lms", (_, res) => {
-  res.sendFile(path.join(__dirname, "public", "questionsAndEntrances.html"));
-});
-
-// API: Добавление нового HTML-вопроса
 router.post("/new-html-question", htmlQuestionController.createQuestion);
-
-// API: Получение всех HTML-вопросов
 router.get("/get-the-htmlQuestions", htmlQuestionController.getQuestion);
-
-// Сценарий по ID
 router.get("/s/:id", IdsController.getTheScript);
-
-// Создание нового ID
 router.post("/newId", IdsController.createId);
-
-// Удаление ID
 router.delete("/test/:id", IdsController.delete);
-
-// Получение всех ID
 router.get("/all-ids", IdsController.getAll);
 
 export default router;
