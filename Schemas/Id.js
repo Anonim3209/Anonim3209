705dce3331fdf720ccca91520242e21737396090
Skirk <86675968+Anonim3209@users.noreply.github.com>
2026-01-03 18:45:58 +0500

    Delete Schemas directory

--- Schemas/Id.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const Id = new mongoose.Schema({
-  idOFLink: { type: String, required: true },
-});
-
-export default mongoose.model("Id", Id);
--- Schemas/htmlQuestion.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const htmlQuestion = new mongoose.Schema({
-  html: { type: String, required: true },
-  timeOfActivation: { type: String, required: true },
-  username: { type: String, required: true },
-});
-
-export default mongoose.model("htmlQuestion", htmlQuestion);
